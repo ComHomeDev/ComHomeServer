6cb8acd9e970cf8dc84ba4ae956463bdde1a09f2
Gongdongjang <jeesoo0908@gmail.com>
2022-07-27 23:48:04 +0900

    [add] secret comment

--- routes/edu_cont_comment.js
+++ routes/edu_cont_comment.js
@@ -10,11 +10,13 @@ router.post("/edu_cont_comment_write", async (req, res) => {
   const user = req.user.id;
   const comment = post.edu_contest_comment_cont;
   const cont_no = post.edu_contest_no;
+  let secret_check = post.secret_check;
+  secret_check = parseInt(secret_check);
 
   try {
     const data = await pool.query(
-      `INSERT INTO edu_contest_comment(edu_contest_comment_cont, iduser, edu_contest_no) VALUES(?, ?, ?)`,
-      [comment, user, cont_no]
+      `INSERT INTO edu_contest_comment(edu_contest_comment_cont, iduser, edu_contest_no, secret_check) VALUES(?, ?, ?, ?)`,
+      [comment, user, cont_no, secret_check]
     );
     res.writeHead(302, {
       Location: "/edu_contest_detail/" + cont_no,
--- routes/edu_contest_detail.js
+++ routes/edu_contest_detail.js
@@ -7,13 +7,23 @@ const path = require("path");
 
 router.get("/edu_contest_detail/:edu_contest_no", async (req, res) => {
   const edu_contest_no = path.parse(req.params.edu_contest_no).base;
-  //   console.log(edu_contest_no);
   const title = edu_contest_no + "번 게시글";
   const head = ``;
   const data = await pool.query(
     `SELECT * FROM edu_contest where edu_contest_no = ?`,
     [edu_contest_no]
   );
+
+  function getCheckboxValue(event) {
+    let result = "";
+    if (event.target.checked) {
+      result = event.target.value;
+    } else {
+      result = "";
+    }
+
+    document.getElementById("my_checkbox").innerText = result;
+  }
 
   let body = `<p>${data[0][0].edu_contest_title}</p> 
   <p>${data[0][0].edu_contest_cont}</p>
@@ -27,7 +37,6 @@ router.get("/edu_contest_detail/:edu_contest_no", async (req, res) => {
     [edu_contest_no]
   );
   let i = 0;
-  // console.log(comment[0][0]);
   if (comment[0][0] == undefined) {
     //댓글 없는 경우
     body += "<p>아직 댓글이 없습니다.</p>";
@@ -37,17 +46,57 @@ router.get("/edu_contest_detail/:edu_contest_no", async (req, res) => {
       const name = await pool.query(`SELECT name FROM user WHERE iduser = ?`, [
         comment[0][i].iduser,
       ]);
-      body += ` <div>
+      if (comment[0][i].secret_check == 0) {
+        //비밀 댓글이 아니라면
+        body += ` <div>
       <div>
-      <span class="comment-content">
-            댓글: ${comment[0][i].edu_contest_comment_cont}
-        </span>
         <div>
-            댓글 작성자: ${name[0][0].name}
+            <!--댓글 작성자: ${name[0][0].name}-->
+            익명
         </div>
+        <span class="comment-content">
+            
+            댓글: ${comment[0][i].edu_contest_comment_cont} 
+        </span>
+        
       </div>
     </div>
     `;
+      } else {
+        //비밀 댓글이라면
+        if (
+          data[0][0].iduser == req.user.id ||
+          req.user.id == comment[0][i].iduser
+        ) {
+          //자신의 게시글이거나 본인이 쓴 댓글이라면
+          body += ` <div>
+              <div>
+                <div>
+                <!--댓글 작성자: ${name[0][0].name}-->
+                 익명
+                </div>
+              <span class="comment-content">
+                댓글: ${comment[0][i].edu_contest_comment_cont} 
+              </span>        
+            </div>
+          </div>
+        `;
+        } else {
+          //본인이 쓴 댓글이 아니라면
+          body += ` <div>
+          <div>
+            <div>
+            <!--댓글 작성자: ${name[0][0].name}-->
+             익명
+            </div>
+          <span class="comment-content">
+           비밀댓글입니다. 
+          </span>        
+        </div>
+      </div>
+    `;
+        }
+      }
       i++;
     }
   }
@@ -55,15 +104,13 @@ router.get("/edu_contest_detail/:edu_contest_no", async (req, res) => {
     comment_write = `자신의 게시글에는 댓글을 작성할 수 없습니다.`;
   } else {
     comment_write += `<form class="comment" action="/edu_cont_comment_write" method="POST">
-        댓글 입력:
-      
-        <input name="edu_contest_comment_cont" placeholder="여기에 댓글을 입력해주세요"></input>
-        
+        <input type="checkbox" id='my_checkbox' /> 
+        비밀댓글 <input name="edu_contest_comment_cont" placeholder="여기에 댓글을 입력해주세요"></input>
         <input name="edu_contest_no" type="hidden" value="${edu_contest_no}">
-        <br>
-        <button type="submit">댓글 입력</button>
-      
-      </form>`;
+        <br> 
+        <input type = "hidden" name = "secret_check" value = "1" onclick="getCheckboxValue(event)"/>
+        <button type="submit">입력</button>
+        </form>`;
   }
 
   body += ` ${comment_write} <br> <a href = "/edu_contest_list">목록으로 돌아가기</a><br>  `;
